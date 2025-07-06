d1b7c7c80749f0e8901c05fd9ee27cf3b07eb5d5
mvicenss <marinapaula.vicensmartin@usp.ceu.es>
2025-07-06 19:02:38 +0200

    Google Analytics added

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,15 +1,29 @@
 "use client";
+import Script from "next/script";
 
 export function Analytics() {
-	const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
-	if (!token) {
-		return null;
-	}
+	const beamToken = process.env.NEXT_PUBLIC_BEAM_TOKEN;
 	return (
-		<script
-			src="https://beamanalytics.b-cdn.net/beam.min.js"
-			data-token={token}
-			async
-		/>
+		<>
+		{beamToken && (
+                <script
+                    src="https://beamanalytics.b-cdn.net/beam.min.js"
+                    data-token={beamToken}
+                    async
+                />
+            )}
+		<Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-PJ28W1P9D2"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-PJ28W1P9D2');
+                `}
+            </Script>
+		</>
 	);
 }
